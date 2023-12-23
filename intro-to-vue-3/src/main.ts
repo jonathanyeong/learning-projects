a6d9847bf1488966f7665890096583d8c032a7e3
Jonathan Yeong <jonathanyeong@gmail.com>
2023-12-23 13:49:11 -0500

    Add description

--- intro-to-vue-3/src/main.ts
+++ intro-to-vue-3/src/main.ts
@@ -2,10 +2,16 @@ import { createApp } from "vue/dist/vue.esm-bundler.js";
 import './style.css'
 
 // Heart of our vue application
-createApp({
+createApp(
+  // Create app takes an option object
+  // Data is one of those options
+  {
   data() {
     return {
-      product: "Socks"
+      // This data is reactive. So changing product will update the DOM.
+      // Every place we refer to {{ product }} will be updated
+      product: "Socks",
+      description: "A pair of warm, fuzzy socks"
     };
   }
 })
